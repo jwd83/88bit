89f8972348313f13c2425d5af0a4fe2534bace1d
Jared De Blander <jared0x90@gmail.com>
2024-12-05 04:17:16 -0500

    working on register file for permanent outputs where relevant (r3 comparison and rio)

--- prelude/registers.sv
+++ prelude/registers.sv
@@ -6,10 +6,24 @@ module registers(
     input logic [7:0] in,
     input logic clk,
     output logic [7:0] out_a,
-    output logic [7:0] out_b
-);
+    output logic [7:0] out_b,
+    output logic [7:0] r3_out;
+    output logic [7:0] rio_out;
 
+);
     logic [7:0] rfile [7:0];
+
+    always_comb begin
+        // hardwire r3_out to rfile[3]
+        // hardwire rio_out to rfile[7]
+        r3_out = rfile[3];
+        rio_out = rfile[7];
+
+        // output the value of the selected registers
+        out_a = rfile[src_a];
+        out_b = rfile[src_b];
+    end
+
 
     always_ff @(posedge clk)
     begin

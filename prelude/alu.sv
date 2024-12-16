956f004203463ef2f743b59e1a6e96c084e8f996
Jared De Blander <jared0x90@gmail.com>
2024-12-15 21:32:43 -0500

    ALU cleanup

--- prelude/alu.sv
+++ prelude/alu.sv
@@ -1,19 +1,28 @@
+/*
+
+ALU operations for Prelude are defined as follows:
+
+----------|-----------|----------------------------------------
+  ALU op  |           |
+  bits    | Operation | Description
+----------|-----------|----------------------------------------
+   000000 | OR        | out = in_a or in_b
+   000001 | NAND      | out = ~(in_a and in_b)
+   000010 | NOR       | out = ~(in_a or in_b)
+   000011 | AND       | out = in_a and in_b
+   000100 | ADD       | out = in_a + in_b
+   000101 | SUB       | out = in_a - in_b
+   000110 | XOR       | out = in_a ^ in_b, Prelude specific
+   000111 | SHL       | out = in_a << in_b, Prelude specific
+----------|-----------|----------------------------------------
+
+*/
 module alu (
     input logic [5:0] alu_op,
     input logic [7:0] in_a,
     input logic [7:0] in_b,
     output logic [7:0] out
-) ;
-
-
-// 000000|OR|out = in_a or in_b
-// 000001|NAND|out = ~(in_a and in_b)
-// 000010|NOR|out = ~(in_a or in_b)
-// 000011|AND|out = in_a and in_b
-// 000100|ADD|out = in_a + in_b
-// 000101|SUB|out = in_a - in_b
-// 000110|XOR|out = in_a ^ in_b, Prelude specific
-// 000111|SHL|out = in_a << in_b, Prelude specific
+);
     always_comb begin
         casez (alu_op)
             6'b000000: out = in_a | in_b;
@@ -27,7 +36,4 @@ module alu (
             default: out = 8'b0;
         endcase
     end
-
-
-
 endmodule

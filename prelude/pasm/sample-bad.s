3bb0d1bff45f497f5e49b1abd15f8ce6db395e86
Jared De Blander <jared0x90@gmail.com>
2024-12-16 21:14:31 -0500

    bad assembly updates

--- prelude/pasm/sample-bad.s
+++ prelude/pasm/sample-bad.s
@@ -2,14 +2,14 @@
 ; full line comment
 ; put some weird spacing on operands to make sure we parse it correctly
 start:
-LOAD 5          ; load 5 into R0
-COPY     R0 R1  ; R1 = R0 (5)
-LOAD 3          ; load 3 into R0
-COPY    R0  R2  ; R2 = R0 (3)
+    LOAD 5          ; load 5 into R0
+    COPY     R0 R1  ; R1 = R0 (5)
+    LOAD 3          ; load 3 into R0
+    COPY    R0  R2  ; R2 = R0 (3)
 add_more:
-ADD             ; R3 = R1 + R2
-COPY R3 RIO     ; RIO = R3
-COPY R3 R1      ; R1 = R3
-add_more:       ; a duplicate label should break the assembly
-LOAD add_more   ; load add_more into R0
-BRA             ; branch always
+    ADD             ; R3 = R1 + R2
+    COPY R3 RIO     ; RIO = R3
+    COPY R3 R1      ; R1 = R3
+add_more: ; !!!!! a duplicate label should break the assembly  !!!!!!
+    LOAD add_more   ; load add_more into R0
+    BRA             ; branch always

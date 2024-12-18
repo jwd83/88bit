dd04cfe6804b0ac6d80c32106f9031eabf9eb964
Jared De Blander <jared0x90@gmail.com>
2024-12-18 01:34:12 -0500

    gowin project

--- prelude/gowin/prelude/src/prelude.sv
+++ prelude/gowin/prelude/src/prelude.sv
@@ -12,7 +12,7 @@ module prelude(
     input logic clk,
     input logic reset,
     input logic [7:0] rio_in,
-    output logic [7:0] rio_out,
+    output logic [7:0] rio_out
 );
 
     logic [7:0] pc; // program counter

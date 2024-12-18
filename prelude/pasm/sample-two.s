8ab8ea2ce79f403640bb4b3b5fb71ec981a87ad0
Jared De Blander <jared0x90@gmail.com>
2024-12-18 05:50:05 -0500

    updating rom.txt with loop

---
+++ prelude/pasm/sample-two.s
@@ -0,0 +1,16 @@
+; full line comment
+const one 1 ; in-line comment
+const two 2
+
+start:
+    LOAD    one         ; load 5 into R0
+    COPY    R0  R1      ; R1 = R0 (5)
+    LOAD    two         ; load THREE into R0
+    COPY    R0  R2      ; R2 = R0 (3)
+
+add_more:
+    ADD             ; R3 = R1 + R2
+    COPY R3 RIO     ; RIO = R3
+    COPY R3 R1      ; R1 = R3
+    LOAD add_more   ; load add_more into R0
+    BRA             ; branch always to address in R0

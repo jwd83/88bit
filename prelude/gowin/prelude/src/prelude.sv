4cbaa781bca19d0f537e14cf10a4b7eb3bcee7d9
Jared De Blander <jared0x90@gmail.com>
2024-12-18 03:29:18 -0500

    constraints work

--- prelude/gowin/prelude/src/prelude.sv
+++ prelude/gowin/prelude/src/prelude.sv
@@ -1,16 +1,32 @@
 module top(
     input logic clk,
-    input logic reset,
-    input logic button,
-    output logic [7:0] leds
+    input logic S1,
+    input logic S2,
+    output logic LED0,
+    output logic LED1,
+    output logic LED2,
+    output logic LED3,
+    output logic LED4,
+    output logic LED5,
 );
+
+    logic [7:0] leds;
 
     prelude prelude(
         .clk(clk),
-        .reset(reset),
-        .rio_in({6'b0, button}),
-        .rio_out(rio_out)
+        .reset(S1),
+        .rio_in({7'b0, S2}),
+        .rio_out(leds)
     );
+
+    always_comb begin
+        LED0 = leds[0];
+        LED1 = leds[1];
+        LED2 = leds[2];
+        LED3 = leds[3];
+        LED4 = leds[4];
+        LED5 = leds[5];
+    end
 
 endmodule
 

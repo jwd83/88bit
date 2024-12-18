781165d25d1a6e15b58bc58dfe1c9530945fd5f3
Jared De Blander <jared0x90@gmail.com>
2024-12-18 05:11:04 -0500

    more changes

--- prelude/prelude.sv
+++ prelude/prelude.sv
@@ -313,7 +313,7 @@ endmodule
 
 /*
 
-Read only memory uses rom.txt to generate the program ROM for Prelude
+Prelude's ROM module
 
 */
 
@@ -321,14 +321,23 @@ module rom (
     input logic [7:0] address,
     output logic [7:0] data
 );
-    logic [7:0] rom_contents [0:255];
-
-    // load our program ROM from rom.txt
-    initial begin
-        $readmemb("rom.txt", rom_contents);
-    end
 
     always_comb begin
-        data = rom_contents[address];
+        case (address)
+            8'h00: data = 8'b10110001;
+            8'h01: data = 8'b10001010;
+            8'h02: data = 8'b01000100;
+            8'h03: data = 8'b10011001;
+            8'h04: data = 8'b01000100;
+            8'h05: data = 8'b10011001;
+            8'h06: data = 8'b01000100;
+            8'h07: data = 8'b10011001;
+            8'h08: data = 8'b01000100;
+            8'h09: data = 8'b10011001;
+            8'h0A: data = 8'b01000100;
+            8'h0B: data = 8'b10011110;
+
+            default: data = 8'b00000000;
+        endcase
     end
 endmodule

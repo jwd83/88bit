5d88bff367290fedb6dbf6702ca1b1ceb6166561
Jared De Blander <jared@ayrscott.com>
2024-12-19 16:07:20 -0500

    work on limb

--- limb/limb.sv
+++ limb/limb.sv
@@ -7,6 +7,26 @@ Written by Jared De Blander in December of 2024
 This is designed to be a simple 8 bit RISC CPU inspired by "LEG" from the hit
 Steam game "Turing Complete" which itself was inspired by "ARM".
 
++-----------------+
+| Register Layout |
++-----------------+
+
+16 general purpose registers
+r0 is hardwired to 0
+r15 is the RAM address register for load/store
+
+special registers
+pc - program counter
+ir - instruction register
+sp - stack pointer register (handled in hardware, not user accessible)
+
++-----------------+
+| Memory Layout   |
++-----------------+
+
+Limb uses a Harvard architecture with dedicated program ROM and a separate RAM
+for variable storage and an internal call stack for call/ret PC tracking.
+
 */
 
 module limb(
@@ -22,12 +42,20 @@ module limb(
     logic [7:0] alu_out;    // alu result
     logic condition_result; // branching signals
     logic write_enable;     // register file signals
-    logic [2:0] src_a;      // register file signals
-    logic [2:0] src_b;      // register file signals
-    logic [2:0] dst;        // register file signals
+    logic [3:0] src_a;      // register file signals
+    logic [3:0] src_b;      // register file signals
+    logic [3:0] dst;        // register file signals
     logic [7:0] in;         // register file signals
     logic [7:0] out_a;      // register file signals
     logic [7:0] out_b;      // register file signals
+
+    // instantiate modules
+    rom rom ();
+    decoder decoder ();
+    registers registers ();
+    ram ram ();
+    alu alu ();
+
 
 
 endmodule
@@ -35,7 +63,11 @@ endmodule
 module decoder();
 endmodule
 
+// 16 registers r0-r15.
+// r0 is always 0
+// r15 sets load/store address from RAM
 module registers(
+    logic [3:0] src_a,
 
 );
 

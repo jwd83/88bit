93ccf116b973283c278d166f627f132bc2b505c4
Jared De Blander <jared0x90@gmail.com>
2024-12-24 15:41:57 -0500

    isa defined for sr16

--- sr16/sr16.sv
+++ sr16/sr16.sv
@@ -2,4 +2,8 @@
 
 The ShinyRock16
 
+My ISA Defined here:
+
+https://docs.google.com/spreadsheets/d/e/2PACX-1vTi-MPLhr4D1eNBMzdLoQ9VZIzOb1GxG1HfwbSVVtuh3BAOuYNPyyuVTR7AJ4V3hfcSsUTbSlYO6jC3/pubhtml
+
 */
